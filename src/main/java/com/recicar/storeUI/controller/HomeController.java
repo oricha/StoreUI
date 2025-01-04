2780acb3fd3ab30181a91683afa37bd45560ef43
Karel <oricha@gmail.com>
2025-01-04 21:51:41 +0100

    Added new endpoint route

--- src/main/java/com/recicar/storeUI/controller/HomeController.java
+++ src/main/java/com/recicar/storeUI/controller/HomeController.java
@@ -1,35 +1,103 @@
 package com.recicar.storeUI.controller;
 
-import com.recicar.storeUI.domain.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public ModelAndView displayArticle(Map<String, Object> model) {
-
-        List<Article> articles = IntStream.range(0, 10)
-                .mapToObj(i -> generateArticle("Article Title " + i))
-                .collect(Collectors.toList());
-
-        model.put("articles", articles);
-        return new ModelAndView("index", model);
+    @GetMapping("/about")
+    public String about() {
+        return "about";
     }
 
-    private Article generateArticle(String s) {
-        Article article = new Article();
-        article.setTitle(s);
-        article.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
-        article.setAuthor("John Doe");
-        article.setPublishDate("2020-01-01");
-        return article;
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/blog-fullwidth")
+    public String blogFullwidth() {
+        return "blog-fullwidth";
+    }
+
+    @GetMapping("/blog-sidebar")
+    public String blogSidebar() {
+        return "blog-sidebar";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "cart";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout() {
+        return "checkout";
+    }
+
+    @GetMapping("/coming-soon")
+    public String comingSoon() {
+        return "coming-soon";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/faq")
+    public String faq() {
+        return "faq";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/index-2")
+    public String index2() {
+        return "index-2";
+    }
+
+    @GetMapping("/index-3")
+    public String index3() {
+        return "index-3";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/my-account")
+    public String myAccount() {
+        return "my-account";
+    }
+
+    @GetMapping("/privacy-policy")
+    public String privacyPolicy() {
+        return "privacy-policy";
+    }
+
+    @GetMapping("/product-details")
+    public String productDetails() {
+        return "product-details";
+    }
+
+    @GetMapping("/services")
+    public String services() {
+        return "services";
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop";
+    }
+
+    @GetMapping("/wishlist")
+    public String wishlist() {
+        return "wishlist";
     }
 }
